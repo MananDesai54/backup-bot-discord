a7947ae3bc40287ab63d1ee30da1ecd0fd410b7e
MananDesai54 <manan5401desai@gmail.com>
2021-09-19 15:14:26 +0530

    error handling

--- bot.py
+++ bot.py
@@ -25,15 +25,15 @@ if __name__ == "__main__":
     async def unload(ctx: Context, extension):
         client.unload_extension(f"cogs.{extension}")
 
+    for filename in os.listdir('./cogs'):
+        if filename.endswith(".py"):
+            client.load_extension(f"cogs.{filename[:-3]}")
+
     # to use run `$reload filename(without .py) from cogs folder`
     @client.command()
     async def reload(ctx: Context, extension):
         client.unload_extension(f"cogs.{extension}")
         client.load_extension(f"cogs.{extension}")
-
-    for filename in os.listdir('./cogs'):
-        if filename.endswith(".py"):
-            client.load_extension(f"cogs.{filename[:-3]}")
 
     # connectMongoDB()
 
--- cogs/BackupBotCore.py
+++ cogs/BackupBotCore.py
@@ -1,7 +1,12 @@
 import discord
-from discord.ext import commands
+from discord.ext import commands, tasks
 from discord.ext.commands.bot import Bot
 from discord.ext.commands.context import Context
+from itertools import cycle
+
+from discord.ext.commands.errors import CommandError
+
+status = cycle(['Looking for Backups', "Restoring Backups"])
 
 
 class BackupBotCore(commands.Cog):
@@ -14,7 +19,19 @@ class BackupBotCore(commands.Cog):
         await self.client.change_presence(
             status=discord.Status.idle,
             activity=discord.Game("Looking for backups"))
+        self.update_status.start()
         print(f"We have logged in as {self.client.user}")
+
+    @commands.Cog.listener()
+    async def on_command_error(self, ctx: Context, error: CommandError):
+        if isinstance(error, commands.MissingRequiredArgument):
+            await ctx.send("Please Provide all required arguments")
+        elif isinstance(error, commands.CommandNotFound):
+            await ctx.send(
+                "Sorry That command not found, Please try $help to get list of commands"
+            )
+        else:
+            print(error)
 
     # Commands
     @commands.command(aliases=['hello', 'hey'])
@@ -22,8 +39,13 @@ class BackupBotCore(commands.Cog):
         await ctx.send(f"Pong {round(self.client.latency * 1000)}ms")
 
     @commands.command()
-    async def clear(self, ctx: Context, amount=5):
+    async def clear(self, ctx: Context, amount: int):
         await ctx.channel.purge(limit=amount)
+
+    # Tasks
+    @tasks.loop(seconds=10)
+    async def update_status(self):
+        await self.client.change_presence(activity=discord.Game(next(status)))
 
 
 def setup(client: Bot):
